f1ed9b6995446aa8add4b152a7c44bc70bd9d3f7
Junaid Iqbal <junaid_mcs89@hotmail.com>
2022-03-29 23:21:06 +0500

    Frist Commit

---
+++ app/Models/Vehicle_Facilities.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle_Facilities extends Model
+{
+    use HasFactory;
+}
